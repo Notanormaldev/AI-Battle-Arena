546fef6b2daa65ab23bed1e2e512e66931366313
Notanormaldev <harshpatel20050@gmail.com>
2026-05-19 16:12:50 +0530

    feat: update dependencies and enhance graph AI service with new state management and scoring logic

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
-
+import morgan from 'morgan';
+import usegraph from './services/graph.ai.service.js'
 const app= express();
+app.use(express.json())
+app.use(morgan('dev'))
 app.get('/health',(req,res)=>{
     res.status(200).json({msg:'ok'})
+})
+
+app.post('/usegraph',async (req,res)=>{
+    await usegraph('what is  factorial')
+    
 })
 export default app;
--- backend/src/services/graph.ai.service.ts
+++ backend/src/services/graph.ai.service.ts
@@ -1,3 +1,89 @@
-import { StateSchema, MessagesValue,  StateGraph, START, END } from "@langchain/langgraph";
+import { HumanMessage } from "@langchain/core/messages";
+import { StateSchema, MessagesValue, StateGraph, START, END, type GraphNode, ReducedValue } from "@langchain/langgraph";
+import {z} from 'zod'
+import { coheremodel, geminimodel, mistralmodel } from "./model.service.js";
+import { createAgent,providerStrategy } from "langchain";
 
 
+
+const State = new StateSchema({
+  messages: MessagesValue,
+  solution_1:new ReducedValue(z.string().default(""),{
+    reducer:(current,next)=>{
+        return next
+    }
+  }),
+  solution_2:new ReducedValue(z.string().default(""),{
+    reducer:(current,next)=>{
+        return next
+    }
+  }),
+  judge_recom:new ReducedValue(z.object().default({
+    solution_1_score:0,
+    solution_2_score:0
+  }),{
+    reducer:(current,next)=>{
+        return next
+    }
+  })
+});
+
+const solutionnode:GraphNode<typeof State> =async(state:typeof State)=>{
+  const [mistral_solution,cohere_solution]=await Promise.all([
+    mistralmodel.invoke(state.messages[0].text),
+    coheremodel.invoke(state.messages[0].text)
+  ])
+  console.log(state);
+  
+  return{
+    solution_1:mistral_solution.text,
+    solution_2:cohere_solution.text
+   }
+   
+}
+const judgenode:GraphNode<typeof State>=async (state:typeof State)=>{
+    const {solution_1,solution_2}=state;
+    const judge = await createAgent({
+        model:geminimodel,
+        tools:[],
+        responseFormat:providerStrategy(z.object({
+            solution_1_score:z.number().min(0).max(10),
+            solution_2_score:z.number().min(0).max(10)
+        }))
+    })
+    const judgeresponse = await judge.invoke({
+        messages:[
+        new HumanMessage(
+            `you are a judge between two solutions for the question ${state.messages[0].text}. solution 1 is ${solution_1} and solution 2 is ${solution_2}. give score out of 10 for each solution based on correctness and conciseness.`
+        )
+    ]
+    })
+   const result = judgeresponse.structuredResponse
+   console.log(state);
+   
+    return {
+       judge_recom:result
+    }
+}
+
+const graph=new StateGraph(State)
+.addNode('solution',solutionnode)
+.addNode('judge',judgenode)
+.addEdge(START,"solution")
+.addEdge("solution","judge")
+.addEdge("judge",END)
+.compile()
+
+
+
+export default async function (usermessage:string){
+    const result = await graph.invoke({
+        messages:[
+            new HumanMessage(usermessage)
+        ]
+    })
+    console.log(result);
+    return result.messages
+
+    
+}
