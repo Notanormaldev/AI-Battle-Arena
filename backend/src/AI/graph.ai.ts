713f992e6ab20bbf0c3138ed06d399c772dfe5cd
Notanormaldev <harshpatel20050@gmail.com>
2026-05-21 18:58:45 +0530

    feat: add judge node to evaluate solutions and provide scores

--- backend/src/AI/graph.ai.ts
+++ backend/src/AI/graph.ai.ts
@@ -1,7 +1,8 @@
-import { StateGraph,StateSchema,type GraphNode } from "@langchain/langgraph";
+import { START, StateGraph,StateSchema,type GraphNode } from "@langchain/langgraph";
 import z from 'zod'
-import { coheremodal, mistralmodal } from "./model.ai.js";
-
+import { coheremodal, geminimodal, mistralmodal } from "./model.ai.js";
+import {createAgent ,HumanMessage,providerStrategy} from "langchain"
+,
 const state = new StateSchema({
    problem: z.string().default(''),
    solution_1:z.string().default(''),
@@ -17,7 +18,7 @@ const state = new StateSchema({
 
 
 
-const solution_node:GraphNode<typeof state>=async(state)=>{
+const solutionnode:GraphNode<typeof state>=async(state)=>{
 
     const [mistralresponse, cohereresponse]=await Promise.all([
         mistralmodal.invoke(state.problem),
@@ -29,3 +30,41 @@ const solution_node:GraphNode<typeof state>=async(state)=>{
         solution_2:coheremodal
     }
 }
+const judgenode:GraphNode<typeof state>=async(state)=>{
+    const {problem,solution_1,solution_2}=state,
+
+
+    const agent = createAgent({
+        model:geminimodal,
+        responseFormat:providerStrategy(z.object({
+            solution_1_score:z.number().min(0).max(10),
+            solution_2_score:z.number().min(0).max(10),
+            solution_1_reason:z.string(),
+            solution_2_reason:z.string(),
+        })),
+        systemPrompt:`you are a judge that compares two solutions to a problem and gives them a score from 0 to 10, and also provides a reason for the score. The problem is ${problem}. The first solution is ${solution_1}. The second solution is ${solution_2}. Please provide your scores and reasons`
+    })
+
+
+     const judgerespons= await agent.invoke({
+        messages:[new HumanMessage(
+            `
+            problem:${problem},
+            solution_1:${solution_1},
+            solution_2:${solution_2},
+            please evaluate the two solutions and provide your scores and reasons`
+        )
+        ]
+     })
+
+
+     const {solution_1_reason,solution_1_score,solution_2_reason,solution_2_score}=judgerespons.structuredResponse
+}
+
+const graph = new StateGraph(state)
+.addNode('solutionnode',solutionnode)
+.addNode('judgenode',judgenode)
+.addEdge(START,"solutionnode")
+.addEdge("solutionnode","judgenode")
+.addEdge("judgenode",END)
+.compile()
