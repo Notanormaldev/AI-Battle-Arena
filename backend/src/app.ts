ef6fccbad9b35c058d15f5b54849812116c98690
Notanormaldev <harshpatel20050@gmail.com>
2026-05-22 18:13:50 +0530

    clear

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,8 +4,18 @@ import rungarph from './AI/graph.ai.js';
 
 
 const app=express();
-app.get('/',async(req,res)=>{
-    const result = await rungarph("write a factorial code")
+app.use(express.json())
+// app.get('/',async(req,res)=>{
+//     const result = await rungarph("write a factorial code")
+
+//     return res.status(200).json({
+//         result
+//     })
+// })
+app.post('/invoke',async (req,res)=>{
+    const {input}=req.body
+
+    const result = await rungarph(input)
 
     return res.status(200).json({
         result
